8373a38ac62433ec4ba24801d03e29df9c1bb95d
Ilnur <cheaz910@mail.ru>
2020-09-27 00:35:11 +0500

    Updates

---
+++ precache-manifest.38fac484d0b152f8a9a5c1a4f3972957.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61789092d1e7faefa088072e9295e108",
+    "url": "/helloworld2/index.html"
+  },
+  {
+    "revision": "0ec4794499233b22f75b",
+    "url": "/helloworld2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0418e48fd01f6de274b2",
+    "url": "/helloworld2/static/js/2.bb6d5c23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helloworld2/static/js/2.bb6d5c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec4794499233b22f75b",
+    "url": "/helloworld2/static/js/main.e4389e6e.chunk.js"
+  },
+  {
+    "revision": "71fe6e3ac68e4fc5afcc",
+    "url": "/helloworld2/static/js/runtime-main.83456281.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloworld2/static/media/logo.5d5d9eef.svg"
+  }
+]);
